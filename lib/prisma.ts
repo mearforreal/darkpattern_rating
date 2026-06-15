3ab3206796904b6b9085e87c734def4c15b529a9
mearforreal <meirerbol@gmail.com>
2026-06-15 13:57:04 -0400

    postgre

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import path from "path";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 function createClient() {
-  const dbUrl = path.resolve(process.cwd(), "dev.db");
-  const adapter = new PrismaBetterSqlite3({ url: dbUrl });
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
   return new PrismaClient({ adapter });
 }
 
